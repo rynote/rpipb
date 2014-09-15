ddb5222b3f1f642ddd6d8b8ba4f4db0c168474b0
Ryan <ryan.gold@gmail.com>
2014-09-15 15:46:03 -0700

    added QRdetective.py for future features

---
+++ QRdetective.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+
+def detect():
+    """Detects qr code from camera and returns string that represents that code.
+
+    return -- qr code from image as string
+    """
+    subprocess.call(["raspistill -n -t 1 -w 512 -h 512 -o cam.png"],shell=True)
+    out = subprocess.Popen(["zbarimg -D cam.png"], stdout=subprocess.PIPE, stderr=open(os.devnull, 'w'), shell=True).communicate()[0] 
+    qr_code = None
+
+    # out looks like "QR-Code: Xuz213asdY...\n" so you need
+    # to remove it and split into list 
+    if len(out) > 8:
+        out = out.replace('QR-code: ','')
+        qr_code = out.replace('QR-Code:','').split()
+    return qr_code
